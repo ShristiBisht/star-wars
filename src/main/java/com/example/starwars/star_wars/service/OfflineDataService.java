20f34b45a141ac8da0e37299a4cf823eefe01f92
Shristi Bisht <shristibisht@Shristis-MacBook-Air.local>
2025-04-29 23:26:21 +0530

    Clean Code

--- src/main/java/com/example/starwars/star_wars/service/OfflineDataService.java
+++ src/main/java/com/example/starwars/star_wars/service/OfflineDataService.java
@@ -1,47 +1,9 @@
 package com.example.starwars.star_wars.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-
 import com.example.starwars.star_wars.model.SearchResult;
-import com.example.starwars.star_wars.repository.InMemoryCache;
 
-
-
-/**This class is unused, but in case will have to put in cache if backend is up */
-
-@Service
-public class OfflineDataService {
-
-    @Autowired
-    private InMemoryCache inMemoryCache;
-
-    @Cacheable(value = "offlineSearchResults", key = "#type + '-' + #name")
-    public SearchResult fetchData(String type, String name) {
-        SearchResult result = inMemoryCache.get(type, name);
-        System.out.println("Cache in here "+result);
-        if (result != null) {    
-            System.out.println("Cache hit for: " + type + " - " + name);
-        } else {
-            // Log cache miss
-            System.out.println("Cache miss for: " + type + " - " + name);
-        }
-
-        return result;
-    }
-
-     // Update the cache with new or refreshed data
-    public void updateData(String type, String name, SearchResult result) {
-        // Put updated data into the cache
-        inMemoryCache.update(type, name, result);
-        System.out.println("Cache updated for: " + type + " - " + name);
-    }
-
-    // Evict cache entry (useful after data update)
-    @CacheEvict(value = "offlineSearchResults", key = "#type + '-' + #name")
-    public void evictSearchResultFromCache(String type, String name) {
-        System.out.println("Evicting cache for: " + type + " - " + name);
-    }
+public interface OfflineDataService {
+    SearchResult fetchData(String type, String name);
+    void updateData(String type, String name, SearchResult result);
+    void evictSearchResultFromCache(String type, String name);
 }
---
+++ src/main/java/com/example/starwars/star_wars/service/impl/OfflineDataServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.starwars.star_wars.service.impl;
+
+import com.example.starwars.star_wars.model.SearchResult;
+import com.example.starwars.star_wars.repository.InMemoryCache;
+import com.example.starwars.star_wars.service.OfflineDataService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class OfflineDataServiceImpl implements OfflineDataService {
+
+    private static final Logger logger = LoggerFactory.getLogger(OfflineDataServiceImpl.class);
+
+    private final InMemoryCache inMemoryCache;
+
+    @Autowired
+    public OfflineDataServiceImpl(InMemoryCache inMemoryCache) {
+        this.inMemoryCache = inMemoryCache;
+    }
+
+    @Override
+    @Cacheable(value = "offlineSearchResults", key = "#type + '-' + #name")
+    public SearchResult fetchData(String type, String name) {
+        SearchResult result = inMemoryCache.get(type, name);
+        if (result != null) {
+            logger.info("Cache hit for: {} - {}", type, name);
+        } else {
+            logger.info("Cache miss for: {} - {}", type, name);
+        }
+        return result;
+    }
+
+    @Override
+    public void updateData(String type, String name, SearchResult result) {
+        inMemoryCache.update(type, name, result);
+        logger.info("Cache updated for: {} - {}", type, name);
+    }
+
+    @Override
+    @CacheEvict(value = "offlineSearchResults", key = "#type + '-' + #name")
+    public void evictSearchResultFromCache(String type, String name) {
+        logger.info("Evicting cache for: {} - {}", type, name);
+    }
+}
