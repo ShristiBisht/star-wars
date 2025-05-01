cf8d3e60ac3b9023f4ac01fc31cde342e28c6499
Shristi Bisht <shristibisht@Shristis-MacBook-Air.local>
2025-05-01 13:47:24 +0530

    ByPass SSL

--- src/main/java/com/example/starwars/star_wars/service/impl/StarWarsApiClientImpl.java
+++ src/main/java/com/example/starwars/star_wars/service/impl/StarWarsApiClientImpl.java
@@ -4,6 +4,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,9 +26,35 @@ public class StarWarsApiClientImpl implements StarWarsApiClient {
     private static final Logger logger = LoggerFactory.getLogger(StarWarsApiClientImpl.class);
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    private static void disableSslVerification() throws Exception {
+         // Create a TrustManager that trusts all certificates
+         TrustManager[] trustAllCertificates = new TrustManager[]{
+             new X509TrustManager() {
+                 public X509Certificate[] getAcceptedIssuers() {
+                     return null;
+                 }
+ 
+                 public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                 }
+ 
+                 public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                 }
+             }
+         };
+ 
+         // Install the all-trusting trust manager
+         SSLContext sc = SSLContext.getInstance("TLS");
+         sc.init(null, trustAllCertificates, new java.security.SecureRandom());
+         HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+ 
+         // Disable hostname verification
+         HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+     }
+ 
     @Override
     public JsonNode fetch(String endpointUrl) {
         try {
+            disableSslVerification();
             URL url = new URL(endpointUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
