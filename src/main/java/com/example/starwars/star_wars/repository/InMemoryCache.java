25043258742b6ff2f205a4812f409bd42deddd13
Shristi Bisht <shristibisht@Shristis-MacBook-Air.local>
2025-05-01 13:25:55 +0530

    Concurrent HashMap Added

--- src/main/java/com/example/starwars/star_wars/repository/InMemoryCache.java
+++ src/main/java/com/example/starwars/star_wars/repository/InMemoryCache.java
@@ -4,15 +4,14 @@ import org.springframework.stereotype.Repository;
 
 import com.example.starwars.star_wars.model.SearchResult;
 
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
-import java.util.HashMap;
 
 @Repository
 public class InMemoryCache {
     Logger logger = Logger.getLogger("InMemoryCache.class");
 
-    private Map<String, SearchResult> cache = new HashMap<>();
+    private ConcurrentHashMap<String, SearchResult> cache = new ConcurrentHashMap<>();
 
     public void update(String type, String name, SearchResult result) {
         cache.put(type + "-" + name, result);
