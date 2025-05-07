8542e38e733f26cfd98c3022baac97db4f911e12
Shristi Bisht <shristibisht@Shristis-MacBook-Air.local>
2025-05-07 11:21:22 +0530

    proxy added

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchEntityData = async (type, name,offlineMode) => {
   const response = await axios.get(`/api/search`, {
-    params: { type, name,offlineMode },
+    params: { type, name,offlineMode: offlineMode ? 'true' : 'false' },
     withCredentials: true,
   });
   return response.data;
