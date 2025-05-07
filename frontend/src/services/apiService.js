1908272f4f887a0d9a53a783cced5b4ca8f4e381
Shristi Bisht <shristibisht@Shristis-MacBook-Air.local>
2025-05-07 10:36:30 +0530

    Jenkins modified

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchEntityData = async (type, name) => {
-  const response = await axios.get(`http://localhost:8080/search`, {
+  const response = await axios.get(`/api/search`, {
     params: { type, name },
     withCredentials: true,
   });
